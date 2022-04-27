29a733a0d2274251df41800ff8ae28928d4f5b49
dylanDuveau <90617692+dylanDuveau@users.noreply.github.com>
2022-04-27 11:55:59 +0200

    Add files via upload

---
+++ Controleur/Controleur.php
@@ -0,0 +1,21 @@
+<?php
+
+
+require_once '../Public/ModeleVille.php';
+
+$uneVille = new ModeleVille();
+
+if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'GET') {
+    $action = filter_input(INPUT_GET, "action");
+    switch ($action) {
+        case 'remplirVilles':       
+            $codePostal = filter_input(INPUT_GET, 'codePostal');
+            if (isset($codePostal)) {
+                 echo $uneVille->ObtenirVilles($codePostal);
+            }
+            break;
+    }
+}
+
+
+
---
+++ Modele/config.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+define("SERVEURBD","195.221.61.190");
+define("LOGIN","dduveau");
+define("MOTDEPASSE","22Cry8uE");
+define("NOMDELABASE","Snir21_dduveau");
+?>
